0401c5ba4edda6a6bb6bcbbcc38f4276f68b1449
Jannis Ruh <ruh.j81@gmail.com>
2023-05-16 10:34:51 +1000

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pauli Tracker
+
+A library to track Pauli frames through a Clifford circuit.
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+#![doc = include_str!("../README.md")]
+
+#![deny(unsafe_op_in_unsafe_fn)]
+// #![warn(missing_docs)] // turn on when things are more stable
