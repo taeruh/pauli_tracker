c8e28b01ab841144895d979d36c947acdd03ef12
Jannis Ruh <ruh.j81@gmail.com>
2023-05-17 14:15:26 +1000

    Use bit-vec instead of bitvec_simd for now
    The problem of bitvec_simd is that it uses smallvec for its internal
    storage. That is really bad for us, because if we have more than 1024
    frames (which is usually the case for real applications) SmallVec will
    spill its data onto the heap and then the size of BitVec (224bytes =
    1024/8 + 8 + padding) are unused (1024/8 from the SmallVec, 8 from
    additional members in BitVec). The problem for us is that we are going
    to have a lot of those BitVec(s) since 2 BitVec(s) are needed for one
    qubit.

--- src/pauli_frame.rs
+++ src/pauli_frame.rs
@@ -1,12 +1,17 @@
-// so we are currently using bitvec_simd as the bitvector implementation; bitvec_simd
-// uses SIMD if possible; there's no stable safe way in std to use simd stuff, there's
-// only a very low level module std::arch; bitvec_simd uses the crate wide which detects
-// the target_feature(s) (at compile time) and wide itself uses the crate safe_arch
-// which detects the target_arch(itectur) (at compile time); currently we are just
-// trusting them to do things correctly; look into the build.rs where the
-// target_feature(s) are enabled if available
-use bitvec_simd::BitVec;
+use bit_vec::BitVec;
 
+#[derive(Debug, Default)]
 pub struct Frames {
+    // note that we are effectively using an array of array; this wouldn't be optimal if
+    // the inner array has a fixed size (then one could do the usual thing and flatten
+    // the arrays into one array), however, this is not necessarily true for us since we
+    // might continuesly add frames and remove qubits (when it is measured) to reduce
+    // the required memory
     frames: Vec<BitVec>,
+}
+
+impl Frames {
+    pub fn new() -> Self {
+        Frames { frames: vec![BitVec::new()] }
+    }
 }
