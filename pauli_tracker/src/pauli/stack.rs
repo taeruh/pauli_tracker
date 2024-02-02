9fa0fc0b69b00253fbe55e8393706a0b4c693030
Jannis Ruh <ruh.j81@gmail.com>
2024-02-02 17:36:00 +1100

    Put serde alias behind cfg_attr

--- pauli_tracker/src/pauli/stack.rs
+++ pauli_tracker/src/pauli/stack.rs
@@ -22,10 +22,11 @@ use crate::boolean_vector::BooleanVector;
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct PauliStack<T /* : BooleanVector */> {
     /// The Z Pauli mask, i.e., the bits flagging whether there's a Z Pauli.
-    #[serde(alias = "right")] // backwards compatibility (at least when deserializing)
+    // backwards compatibility (at least when deserializing)
+    #[cfg_attr(feature = "serde", serde(alias = "right"))]
     pub z: T,
     /// The X Pauli mask, i.e., the bits flagging whether there's a X Pauli.
-    #[serde(alias = "left")] // backwards comp....
+    #[cfg_attr(feature = "serde", serde(alias = "left"))] // backwards comp....
     pub x: T,
 }
 
