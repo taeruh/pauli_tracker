f80617313c7b0ec44393f24d527240fd2a3907c3
Jannis Ruh <ruh.j81@gmail.com>
2024-10-18 12:06:17 +1100

    Python-Release 0.1.2

--- python_lib/CHANGELOG.md
+++ python_lib/CHANGELOG.md
@@ -16,18 +16,19 @@ We (try to) follow the [SemVer] rules.
 
 ## [Unreleased]
 ### Added
-- Add the `Tracker::remove_*` methods.
-- Add the `Tracker::zc*` methods.
-- Add `PauliStack`s `(take_)into_py_bool_tuple` methods.
 ### Changed
 ### Deprecated
 ### Removed
 ### Fixed
 ### Security
 
+## [0.1.2] - 2024-10-18
+Various updates and fixes.
+
 ## [0.1.0] - 2024-02-12
 
-[Unreleased]: https://github.com/taeruh/pauli_tracker/compare/pv0.1.0...HEAD
+[Unreleased]: https://github.com/taeruh/pauli_tracker/compare/pv0.1.2...HEAD
+[0.1.2]: https://github.com/taeruh/pauli_tracker/compare/pv0.1.0...pv0.1.2
 
 [Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
 [SemVer]: https://semver.org/
--- python_lib/tests/main.py
+++ python_lib/tests/main.py
@@ -2,30 +2,11 @@
 
 
 from pauli_tracker.frames.map import Frames
-from pauli_tracker.pauli import PauliStack
-import pauli_tracker
-
 
 if __name__ == "__main__":
     frames = Frames(3)
-    frames.track_x(0)
-    frames.track_z(1)
-    frames.track_y(2)
-    frames.cy(0, 2)
-    frames.serialize("output/hey.json")
-    print(frames.into_py_dict_recursive())
-    frames.remove_x(0)
-    frames.zcx(1, 2)
-    print(frames.into_py_dict_recursive())
-    frames = Frames.deserialize("output/hey.json")
-    serialized = frames.serialize_to_string()
-    # print(serialized)
-    frames = Frames.deserialize_from_string(serialized)
-    frames.serialize("output/hey.bin", serialization_format="bincode")
-    frames = Frames.deserialize("output/hey.bin", serialization_format="bincode")
-    stacks = frames.into_py_dict()
-    py_stacks = frames.take_into_py_dict_recursive()
-    for key in stacks.keys():
-        assert stacks[key].take_into_py_bool_tuple() == tuple(
-            map(lambda x: pauli_tracker.bitvector_to_boolvector(x, 3), py_stacks[key])
-        )
+    frames.track_x(1)
+    frames.track_x(1)
+    map = [0, 0]
+    graph = frames.get_order(map)
+    print(graph.take_into_py_graph())
